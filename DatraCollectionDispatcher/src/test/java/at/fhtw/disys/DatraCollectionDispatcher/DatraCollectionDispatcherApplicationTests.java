fa1afaf0119be3d0e5061cabdd0ac5932beaae31
wi20b016 <86468027+wi20b016@users.noreply.github.com>
2022-06-24 11:29:22 +0200

    com1
    com1

---
+++ DatraCollectionDispatcher/src/test/java/at/fhtw/disys/DatraCollectionDispatcher/DatraCollectionDispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.disys.DatraCollectionDispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatraCollectionDispatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
